bc099d7c06ba0c0b82683635746fe208ac1dcb23
giovanni <g.diana.mail@gmail.com>
2020-03-05 19:02:18 +0000

    update

--- src/figures.R
+++ src/figures.R
@@ -16,3 +16,24 @@ barplot(rbind(diff(data_conf[country_index,]),
 legend("topright",legend=c("confirmed","recovered","deaths"),fill=c("red","green","purple" ))
 
 dev.off()
+
+# Figure param lambda
+
+par_comb=vector("list",4)
+labels=rep("",nrow(data))
+for(j in 1:nrow(data)) labels[j]<-paste(as.character(country[j,1]),as.character(province[j,1]))
+
+par_comb[[1]]=par_comb[[1]]=unlist(lapply(sam_glob_cut,"[[",'lambda'))
+for(i in 2:4){
+       	par_comb[[i]]=t(do.call(cbind,lapply(sam_glob_cut,function(x) x$param[,i-1])))
+	colnames(par_comb[[i]])=labels
+}
+
+
+
+
+png("../Figures/Figure_stat_1.png",width=600,height=1000)
+means.h=colMeans(par_comb[[2]])
+ord=order(means.h,decreasing=T)
+boxplot(par_comb[[2]][,ord], horizontal=T,las=2)
+dev.off()
